af28c8c05f9455eb846cecba3a31df0cc19846a7
Miki Poker <milanpokrajac87@gmail.com>
2016-06-28 16:08:04 +0100

    updated balance

--- data.py
+++ data.py
@@ -1,15 +1,16 @@
 import sys
 
 f = open("dataprimer.csv","r")
-ln = 0
+
 total=0
 for line in f:
-	if ln > 0:
+	try:
 		line = line.strip("\n")
 		entry = line.split(",")
 		total += int(entry[4])	
-		print total
-	ln +=1
+	except:
+		pass
+
 print total
 
 f.close
